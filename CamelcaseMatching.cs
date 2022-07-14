952452605c750666e5e8eb21e7d51ea1d96fe25c
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-07-13 23:28:24 -0700

    Add files via upload

---
+++ CamelcaseMatching.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trie
+{
+    public class CamelcaseMatching
+    {
+        /*
+         * T.C: O(N) where n is number of quries
+         * S.C: O(1)
+         */
+        public IList<bool> CamelMatch(string[] queries, string pattern)
+        {
+            if (queries == null || queries.Length == 0) return null;
+
+            List<bool> result = new List<bool>();
+
+            foreach (string query in queries)
+            {
+                int index = 0;
+                bool isMatch = false;
+                for (int i = 0; i < query.Length; i++)
+                {
+                    char c = query[i];
+                    if (index < pattern.Length && c == pattern[index])
+                    {
+                        index++;
+                        if (index == pattern.Length)
+                        {
+                            isMatch = true;
+                        }
+                    }
+                    else if (Char.IsUpper(c))
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                result.Add(isMatch);
+            }
+            return result;
+        }
+    }
+}
---
+++ TopKFrequentElement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trie
+{
+    public class TopKFrequentElement
+    {
+        /*
+         * T.C: O(nlogk) where n is numer of element in nums array
+         * s.C: O(k) 
+         */
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            if (nums == null || nums.Length == 0) return new int[] { };
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            int[] result = new int[k];
+
+            foreach (int num in nums)
+            {
+                if (!map.ContainsKey(num))
+                {
+                    map.Add(num, 1);
+                }
+                else
+                {
+                    map[num]++;
+                }
+            }
+
+            PriorityQueue<int[], int> pq = new PriorityQueue<int[], int>();
+
+            foreach (int key in map.Keys)
+            {
+                pq.Enqueue(new int[] { key, map[key] }, map[key]);
+
+                if (pq.Count > k)
+                {
+                    pq.Dequeue();
+                }
+            }
+
+            while (pq.Count != 0 && k > 0)
+            {
+                int[] numArr = pq.Dequeue();
+                Console.WriteLine(k);
+                result[k - 1] = numArr[0];
+                k--;
+            }
+
+            return result;
+        }
+
+
+    }
+}
+
